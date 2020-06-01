6024f044d40205f10292070eefbbafab3ee66580
Paulgof <paulgof@outlook.com>
2020-06-02 00:57:37 +0300

    2.1 v1

---
+++ ask/qa/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from .views import test
+
+urlpatterns = [
+    path('', test, name='home'),
+    path('login/', test, name='login'),
+    path('signup/', test, name='signup'),
+    path('question/<int:id>/', test, name='question'),
+    path('ask/', test, name='ask'),
+    path('popular/', test, name='popular'),
+    path('new/', test, name='new')
+]
--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/site.conf
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/gunicorn.conf.py /etc/gunicorn.d/site.conf.py
-gunicorn -c /etc/gunicorn.d/site.conf.py hello:entrance -D
 sudo /etc/init.d/nginx restart
+gunicorn -b "0.0.0.0:8080" hello:entrance -D
+gunicorn -b "0.0.0.0:8000" ask.wsgi:application --chdir ./ask -D
