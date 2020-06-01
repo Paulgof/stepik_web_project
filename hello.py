317ccd9656ed99c7dcb941545aa57c3ecad5ca5c
Paulgof <paulgof@outlook.com>
2020-06-01 18:38:45 +0300

    1.9 v1

---
+++ hello.py
@@ -0,0 +1,5 @@
+def entrance(env, start_response):
+    data = str(env['RAW_URI'])
+    data = bytes('\n'.join(data[1:].split('&')), encoding='UTF-8')
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    return iter([data])
--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/site.conf
 sudo rm /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/gunicorn.conf.py /etc/gunicorn.d/site.conf.py
+gunicorn -c site.conf.py hello:entrance
 sudo /etc/init.d/nginx restart
