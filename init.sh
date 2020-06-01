0a44c95b3e17380ba72f43f320eff01f8aa65362
Paulgof <paulgof@outlook.com>
2020-06-01 18:52:44 +0300

    1.9 v2

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/site.conf
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/gunicorn.conf.py /etc/gunicorn.d/site.conf.py
-gunicorn -c site.conf.py hello:entrance
+gunicorn -c /etc/gunicorn/site.conf.py hello:entrance
 sudo /etc/init.d/nginx restart
